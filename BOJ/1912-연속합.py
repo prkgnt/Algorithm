e172f8aa57794e3594c312a41a6d81aa8b86fb59
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-01-11 19:04:01 +0900

    1912-연속합

--- BOJ/1912-연속합.py
+++ BOJ/1912-연속합.py
@@ -4,15 +4,13 @@ li = list(map(int, input().split()))
 # dp[i] = i번째까지의 합 중 가장 큰 합
 dp = [-1001]*n
 dp[0] = li[0]
-# 전체 합이 0보다 작지않으면 계속 갱신하면서 나아가기
-# 전까지의 최대값과 현재값의 합이 현재 값보다 크면 갱신
+
 for i in range(1, n):
+    # 전까지의 최대값과 현재값의 합이 현재 값보다 크면 갱신
     if dp[i-1] + li[i] > li[i]:
         dp[i] = dp[i-1] + li[i]
     else:
+        # 전까지의 최대값이 현재값보다 작으면 현재 값을 최대값으로 갱신
         dp[i] = li[i]
 
-
-max = max(dp)
-
-print(max)
+print(max(dp))
