45dad75262e3b874998b95a9e6541c6aeaf391d1
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-01-14 19:05:15 +0900

    주석수정

--- BOJ/11054-가장긴바이토닉부분수열.py
+++ BOJ/11054-가장긴바이토닉부분수열.py
@@ -1,21 +1,22 @@
 n = int(input())
 arr = list(map(int, input().split()))
 arr_reverse = arr[::-1]
-#print(arr_reverse)
 
+# 증가하는 수열의 각 값들 저장
 dp_increase = [0]*n
+# 감소하는 값들의 각 값들 저장
 dp_decrease = [0]*n
 
 for i in range(n):
     for j in range(i):
         if arr[i] > arr[j]:
             dp_increase[i] = max(dp_increase[i], dp_increase[j]+1)
+        # 감소하는 경우 거꾸로 보면 결국 증가하는 수열임
         if arr_reverse[i] > arr_reverse[j]:
             dp_decrease[i] = max(dp_decrease[i], dp_decrease[j]+1)
 
-# print(dp_increase)
-# print(dp_decrease)
 res = []
 for i in range(n):
-    res.append(dp_increase[i] + dp_decrease[n-i-1])
-print(max(res) + 1)
+    # 증가하는 수 + 자기 자신 + 감소하는 수
+    res.append(dp_increase[i] + 1 + dp_decrease[n-i-1])
+print(max(res))
