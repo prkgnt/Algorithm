8569e3e9dffe85c2c490b41652545b6d1e075d71
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-01-19 16:45:01 +0900

    2630

--- BOJ/분할정복/2630-색종이만들기.py
+++ BOJ/분할정복/2630-색종이만들기.py
@@ -1,3 +1,29 @@
+import sys
+sys.setrecursionlimit(10**7)
+
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
-print(arr)
+
+white = 0
+blue = 0
+# x, y = 시작점의 좌표
+def sol(x, y, n):
+    global white
+    global blue
+    color = arr[x][y]
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if arr[i][j] != color:
+                sol(x,y,n//2)
+                sol(x+n//2, y, n//2)
+                sol(x, y+n//2, n//2)
+                sol(x+n//2, y+n//2, n//2)
+                return 1
+    if color == 1:
+        blue += 1
+    else:
+        white += 1
+
+sol(0, 0, n)
+print(white)
+print(blue)
